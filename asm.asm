5704666c938cd8e9b224e14e3552f2e1d087e9e9
Lisardowo <lisardowo@gmail.com>
2026-09-01 09:32:46 -0600

    errors

--- asm.asm
+++ asm.asm
@@ -30,7 +30,7 @@ gdt_code: ; code section for locating the kernel and construct reading
   ; be 16 bytes long
 
   dw 0xFFFF, 0x0
-  db 0x0, 10011010b, 11001111b 0x0
+  db 0x0, 10011010b, 11001111b, 0x0
 
 gdt_data:
   ; functionally code and data maps the same memory layout
@@ -47,7 +47,7 @@ gdt_end:
 
 ; GDT descriptor
 gdt_descriptor:
-  dw gdt_end - gdt start - 1 ; size 16 bit
+  dw gdt_end - gdt_start - 1 ; size 16 bit
   dd gdt_start               ; address (32 bit)
 
 CODE_SEG equ gdt_code - gdt_start
