b7cd5fae6fc8ecee8e7c02a5b66ba1557bd2957c
Lisardowo <lisardowo@gmail.com>
2026-09-15 20:15:37 -0600

    huh

--- kernel.c
+++ kernel.c
@@ -1,8 +1,12 @@
 
 #define VideoMemory (volatile char*)0xB8000
 
+//enum whichColor{
+
+//}; 
+
 void printToKern(char *Message,char color );
-char swapColors(int *j);
+//char swapColors(int *j);
 
 void _start(){
   char color = 0x0F; // white in hex
@@ -22,7 +26,7 @@ void printToKern(char *Message, char color){
   int j = 0;
   for(int i=0; Message[i] != '\0' ; i++){
       *currentAddress++ = Message[i];
-      *currentAddress++ = swapColors(&j);
+      *currentAddress++ = color ;//swapColors(&j);
       /*VideoMemory layout alternates 1 byte for whats to be printed and 1 byte dictates the color
        
        P.E: 0xB8000 -> starts the VideoMemory domain, Byte to be displayed
@@ -37,18 +41,20 @@ void printToKern(char *Message, char color){
 
 }
 
-char swapColors(int *j){
+/* char swapColors(int *j){
   
-  char Colors[] = {0xFF, 0x57, 0x33, -1}; 
+  char Colors[] = {0xFF, 0x57, 0x33}; 
 
-  for(;;*j++){
+  int len = sizeof(Colors) / sizeof(Colors[0])
 
-    if (Colors[*j + 1] == -1) { // Would this be better with null?
-      *j = 0;
-      return Colors[*j];
-    }
+    if (Colors[(*j) + 1] <= len) { // Would this be better with null?
+      (*j) = 0;
+      return Colors[(*j)];
+    //use defines, enums or define for readability (instead of returning the value of j return sum like REDCOLOR)    
+}
 
-    return Colors[*j + 1];
+    return Colors[(*j) + 1];
 
   }
 }
+*/ 
